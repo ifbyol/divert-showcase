1b68e84de88b1003e084e77bd914fca49b465028
Nacho Fuertes <nacho@okteto.com>
2025-08-04 08:37:27 +0200

    Added several services to the compose
    Signed-off-by: Nacho Fuertes <nacho@okteto.com>

---
+++ chain-of-services-via-discovery/servicecv/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+
+const PORT = 8080;
+
+app.get("/", function (req, res) {
+  res.send("Service CV says hello!");
+});
+
+app.get("/chain", function (req, res) {
+  console.log("/chain request");
+  res.send(`Service CV says hello from ${process.env.OKTETO_NAMESPACE}!`);
+});
+
+app.listen(PORT, function () {
+  console.log("Started service-cv server on %d", PORT);
+});
